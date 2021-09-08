778cee9f5d64bd94ce91a3bb788af49172ef1869
Aaro Alhainen <aaro.alhainen@gmail.com>
2021-09-08 21:55:02 +0300

    Initial work for scrollable view widget

---
+++ widgets/ScrollableFrame.py
@@ -0,0 +1,23 @@
+from tkinter import ttk
+import tkinter as tk
+
+class ScrollableFrame(ttk.Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+        canvas = tk.Canvas(self, height=580, width=700)
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        self.scrollable_frame = ttk.Frame(canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
