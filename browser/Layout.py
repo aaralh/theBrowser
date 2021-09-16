e9d7d4172aa8e5460a842295ddf96791e02ff7e0
Aaro Alhainen <aaro.alhainen@gmail.com>
2021-09-16 20:32:31 +0300

    Improve text alignment when font size differs on the same row

--- browser/Layout.py
+++ browser/Layout.py
@@ -10,6 +10,15 @@ class DOMElement():
     element: Element
     font: Font
 
+FONTS = {}
+
+def get_font(size, weight, slant) -> Font:
+    key = (size, weight, slant)
+    if key not in FONTS:
+        font = Font(size=size, weight=weight, slant=slant)
+        FONTS[key] = font
+    return FONTS[key]
+
 class Layout:
     def __init__(self, elements: List[DOMElement], hstep, vstep, width):
         self.display_list = []
@@ -18,20 +27,39 @@ class Layout:
         self.width = width
         self.cursor_x = hstep
         self.cursor_y = vstep
-
+        self.line = []
 
         for element in elements:
             self.layout(element)
 
+        self.flush()
+
     def layout(self, element: DOMElement) -> None:
         if isinstance(element.element, Text):
             self.text(element.element, element.font)
+        elif element.element.name == "br":
+            self.flush()
 
     def text(self, element: Text, font: Font):
+        #font = get_font(self.size, self.weight, self.style)
         for word in element.data.split():
             w = font.measure(word)
             if self.cursor_x + w >= self.width - self.hstep:
-                self.cursor_y += font.metrics("linespace") * 1.2
-                self.cursor_x = self.hstep
-            self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+                self.flush()
+            self.line.append((self.cursor_x, word, font))
             self.cursor_x += w + font.measure(" ")
+
+    def flush(self) -> None:
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+
+        self.cursor_x = self.hstep
+        self.line = []
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
