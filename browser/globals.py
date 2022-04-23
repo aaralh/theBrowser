0f735dd85eef879a398ce427123524b3c0584ece
Aaro Alhainen <aaro.alhainen@gmail.com>
2022-04-23 22:13:14 +0300

    Add new browser global

--- browser/globals.py
+++ browser/globals.py
@@ -1,6 +1,15 @@
-
 HSTEP, VSTEP = 13, 18
 EMOJIS_PATH = "resources/emojis/"
 
 
+class BrowserState():
+    __current_url: str = ""
+
+    @staticmethod
+    def get_current_url() -> str:
+        return BrowserState.__current_url
+
+    @staticmethod
+    def set_current_url(url: str) -> None:
+        BrowserState.__current_url = url
     
