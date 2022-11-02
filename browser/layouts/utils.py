5db77ce36c31a6d371b08a588f36e9fcee617ac4
Aaro Alhainen <aaro.alhainen@gmail.com>
2022-11-02 22:18:54 +0200

    Improve font weight transformation

--- browser/layouts/utils.py
+++ browser/layouts/utils.py
@@ -10,6 +10,9 @@ def font_weight_to_string(weight: Union[Literal['normal', 'bold'], str]) -> Lite
         return "normal"
     elif weight == "bolder":
         weight = "bold"
+    if not weight in ["normal", "bold"]:
+        # TODO: Override all unsupported font weight values.
+        weight = "normal"
     return cast(Literal['normal', 'bold'], weight)
 
 
