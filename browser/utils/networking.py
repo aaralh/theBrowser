8c035d79d87d8023407fb794c001d820d3c29784
Aaro Alhainen <aaro.alhainen@gmail.com>
2022-11-19 10:04:56 +0200

    Add each network request to registered inspectors

--- browser/utils/networking.py
+++ browser/utils/networking.py
@@ -2,16 +2,22 @@ from typing import Optional
 import requests
 from requests.models import Response
 import json
+from browser.Inspector import NetworkRequest
+
+from browser.globals import BrowserState
 
 def request(url: str, payload: Optional[dict]=None) -> Response:
+    inspectors = BrowserState.get_inspectors()
     if url.startswith("http://") or url.startswith("https://"):
         headers = {
                 "User-Agent": "theBrowser/0.03-alpha"
             }
         if payload:
             response = requests.post(url, json=json.dumps(payload), headers=headers)
+            [inspector.add_network_request(NetworkRequest(url, "POST", response.status_code, len(response.content))) for inspector in inspectors]
         else:
             response = requests.get(url, headers=headers)
+            [inspector.add_network_request(NetworkRequest(url, "GET", response.status_code, len(response.content))) for inspector in inspectors]
         return response
 
 
