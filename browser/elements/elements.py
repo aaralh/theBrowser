10c5290d1352f30d04300c9cb7d7d81c409c619f
Aaro Alhainen <aaro.alhainen@gmail.com>
2022-11-20 09:25:06 +0200

    Refactor the code to use color utils

--- browser/elements/elements.py
+++ browser/elements/elements.py
@@ -1,25 +1,11 @@
 from dataclasses import dataclass
 from tkinter import Canvas
-from turtle import color
 from typing import List, cast
 from PIL import ImageTk, Image
-from browser.globals import EMOJIS_PATH
 from tkinter.font import Font
+from typing import NewType
 
-
-def is_valid_color(color: str) -> bool:
-    if color.startswith("#") and any(char in color for char in [" ", ";", ","]):
-        return False
-        
-    if color.startswith("#"):
-        # TODO: Add support for rgba style hex colors.
-        if not len(color[1:]) in [3, 6]:
-            print("Faulty color", color)
-        return len(color[1:]) in [3, 6]
-    return True
-
-def rgb_to_hex(rgb: tuple[int, int, int]) -> str:
-    return '%02x%02x%02x' % rgb
+from browser.styling.color.utils import is_valid_color, rgb_to_hex, transform_color
 
 class DrawImage:
     def __init__(self, x1, y1, height, image: ImageTk):
@@ -42,21 +28,16 @@ class DrawText:
         self.color = color
         self.bottom = y1 + font.metrics("linespace")
         self.used_resources = []
+        self.calculated_color = None
 
     def is_emoji(self, unicode, supported_emojis: List[str]) -> bool:
         return unicode in supported_emojis
 
     def execute(self, scroll: int, canvas: Canvas, supported_emojis: List[str]):
-        # TODO: Do proper implementation for rgb and rgba colors.
-        if self.color == "inherit" or self.color == "transparent" or self.color.startswith("var") or not is_valid_color(self.color):
-            self.color = "pink"
-        
-        if self.color.startswith("rgb"):
-            # TODO: Add support for rgba
-            rgb = cast(tuple[int, int, int], tuple([int(number) for number in self.color.split("(")[-1].split(")")[0].split(",")[:3]]))
-            self.color = "#" + rgb_to_hex(rgb)
+        if not self.calculated_color:
+            self.calculated_color = transform_color(self.color)
 
-        canvas.create_text(self.left, self.top - scroll, text=self.text, font=self.font, anchor='nw', fill=self.color)
+        canvas.create_text(self.left, self.top - scroll, text=self.text, font=self.font, anchor='nw', fill=self.calculated_color.color)
         """if not set(list(self.text)).isdisjoint(set(supported_emojis)):
             canvas.create_text(self.left, self.top - scroll, text=self.text, font=self.font, anchor='nw', fill=self.color)
         else:
@@ -77,7 +58,6 @@ class BorderProperties:
     color: str
     width: int
 
-
 class DrawRect:
     def __init__(self, x1, y1, x2, y2, color, border: BorderProperties = BorderProperties("", 0)):
         self.top = y1
@@ -87,34 +67,24 @@ class DrawRect:
         self.color = color
         self.used_resources = None
         self.border = border
+        self.calculated_color = None
 
     def execute(self, scroll: int, canvas: Canvas, supported_emojis: List[str]):
         # TODO: Do proper implementation for rgb and rgba colors.
-        if self.color == "inherit" or self.color.startswith("var") or not is_valid_color(self.color):
-            self.color = "pink"
+        if not self.calculated_color:
+            self.calculated_color = transform_color(self.color)
         
-        if self.color.startswith("rgba"):
+        if self.calculated_color.type == "rgba_color":
             if not self.used_resources:
-                if ", " in self.color:
-                    rgba = [float(value) for value in self.color[5:-1].split(", ")]
-                else:
-                    rgba = [float(value) for value in self.color[5:-1].split(",")]
-                rgba[3] = 255 * rgba[3]
-                rgba = [int(value) for value in rgba]
-                fill = tuple(rgba)
-                image = Image.new('RGBA', (int(self.right-self.left), int(self.bottom-self.top)), fill)
+                image = Image.new('RGBA', (int(self.right-self.left), int(self.bottom-self.top)), self.calculated_color.color)
                 tk_image = ImageTk.PhotoImage(image)
                 self.used_resources = tk_image
             canvas.create_image((self.left, self.top - scroll), image=self.used_resources, anchor='nw')
-        elif self.color.startswith("rgb"):
-            rgb = cast(tuple[int, int, int], tuple([int(number) for number in self.color.split("(")[-1].split(")")[0].split(",")[:3]]))
-            self.color = "#" + rgb_to_hex(rgb)
-        if not self.color.startswith("rgba"):
-            if self.color == "none": self.color = "pink"
+        if not self.calculated_color.type == "rgba_color":
             canvas.create_rectangle(
                 self.left, self.top - scroll,
                 self.right, self.bottom - scroll,
                 width=self.border.width,
-                fill=self.color,
+                fill=self.calculated_color.color,
                 outline=self.border.color
             )
