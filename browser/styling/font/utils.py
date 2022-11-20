dcc12f49b29a39fb48a1c55d88ec60f7bd43b7dc
Aaro Alhainen <aaro.alhainen@gmail.com>
2022-11-20 15:23:46 +0200

    Add utils for css font sizes

---
+++ browser/styling/font/utils.py
@@ -0,0 +1,8 @@
+from typing import Literal
+
+
+CSS_FONTS_SIZE = ["xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large", "xxx-large"]
+
+def convert_absolute_size_to_pixels(font_size: Literal["xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large", "xxx-large"]) -> int:
+    # TODO: Handle font size conversion.
+    return 16
