74a245b45e9ebcbee5ae6289f2fa7350b25dcc5c
Aaro Alhainen <aaro.alhainen@gmail.com>
2022-11-20 09:26:49 +0200

    Add initial support for border and refactor to  use color utils

--- browser/layouts/Layout.py
+++ browser/layouts/Layout.py
@@ -1,6 +1,7 @@
-from typing import List, Literal
+from typing import List, Literal, Optional, Union
 from browser.elements.elements import BorderProperties, DrawRect
 from browser.globals import BrowserState
+from browser.styling.color.utils import CSS_COLORS
 from web.dom.Node import Node
 from web.dom.elements.Text import Text
 from web.dom.elements.Element import Element
@@ -62,8 +63,12 @@ class Layout:
                 if attr_width.endswith("px"):
                     self.width = int(attr_width.replace("px", ""))
                 elif attr_width.endswith("em"):
+                    font_size: str = self.node.style["font-size"]
+                    if font_size.endswith("%"):
+                        parent_font_size = int(self.parent.node.style["font-size"].replace("px", ""))
+                        font_size = str((parent_font_size / 100) * int(font_size.replace("%", "")))
                     font_size = int(
-                        self.node.style["font-size"].replace("px", ""))
+                        round(float(font_size.replace("px", ""))))
                     self.width = int(attr_width.replace("em", "")) * font_size
                 elif attr_width.endswith("%"):
                     self.dynamic_size = True
@@ -91,11 +96,34 @@ class Layout:
             return "inline"
         else:
             return "block"
-
+    
     def paint(self, display_list: list) -> None:
         if isinstance(self.node, Element):
                 bgcolor = self.node.style.get("background-color",
                                         "transparent")
+                border_style: Optional[str] = self.node.style.get("border", None)
+
+                border = None
+
+                if border_style:
+                    styles = border_style.split(" ")
+                    print("Styles", styles)
+                    color = next(filter(lambda item: item.startswith("#") or item.startswith("rgb") or item in CSS_COLORS, styles), None)
+                    width = next(filter(lambda item: item.endswith("%") or item.endswith("px") or item.endswith("em"), styles), "")
+                    style = None
+                    style_width = 0
+                    if width.endswith("em"):
+                        font_size = int(self.node.style["font-size"].replace("px", ""))
+                        style_width = float(width.replace("em", "")) * font_size
+                    elif width.endswith("%"):
+                        style_width = self.parent.width * (int(width.replace("%", "")) / 100)
+                    elif width.endswith("px"):
+                        style_width = int(width[:-2])
+
+                    print(width, style_width)
+
+                    border = BorderProperties(width=style_width, color=color)
+
 
                 if bgcolor == "unset":
                     try:
@@ -109,12 +137,18 @@ class Layout:
                     x2, y2 = self.x + self.width, self.y + self.height
                     if str(self.node.id) in BrowserState.get_selected_elements():
                         rect = DrawRect(self.x, self.y, x2, y2, bgcolor, BorderProperties("red", 10))
+                    elif border:
+                        rect = DrawRect(self.x, self.y, x2, y2, bgcolor, border)
                     else:
                         rect = DrawRect(self.x, self.y, x2, y2, bgcolor)
                     display_list.append(rect)
                 if str(self.node.id) in BrowserState.get_selected_elements():
-                        x2, y2 = self.x + self.width, self.y + self.height
-                        rect = DrawRect(self.x, self.y, x2, y2, "", BorderProperties("red", 10))
+                    x2, y2 = self.x + self.width, self.y + self.height
+                    rect = DrawRect(self.x, self.y, x2, y2, "", BorderProperties("red", 10))
+                elif border:
+                    x2, y2 = self.x + self.width, self.y + self.height
+                    print("Border color", border.color)
+                    rect = DrawRect(self.x, self.y, x2, y2, "", border)
 
         for child in self.children:
             child.paint(display_list) 
