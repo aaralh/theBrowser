475b71120095c7ab2818129eb335e441689c2356
Aaro Alhainen <aaro.alhainen@gmail.com>
2023-05-01 12:10:11 +0300

    try to detect file encoding before reading

--- browser/utils/networking.py
+++ browser/utils/networking.py
@@ -1,12 +1,14 @@
-from typing import Optional
+from typing import Dict, Optional
 import requests
 from requests.models import Response
 import json
 from browser.Inspector import NetworkRequest
+from charset_normalizer import detect
 
 from browser.globals import BrowserState
 
-REQUEST_CACHE = {}
+REQUEST_CACHE: Dict[str, Response] = {}
+
 
 def request(url: str, payload: Optional[dict]=None) -> Response:
     inspectors = BrowserState.get_inspectors()
@@ -18,9 +20,11 @@ def request(url: str, payload: Optional[dict]=None) -> Response:
             }
         if payload:
             response = requests.post(url, json=json.dumps(payload), headers=headers)
+            response.encoding = "UTF-8"
             [inspector.add_network_request(NetworkRequest(url, "POST", response.status_code, len(response.content))) for inspector in inspectors]
         else:
             response = requests.get(url, headers=headers)
+            response.encoding = "UTF-8"
             [inspector.add_network_request(NetworkRequest(url, "GET", response.status_code, len(response.content))) for inspector in inspectors]
         if not payload:
             REQUEST_CACHE[url] = response
@@ -30,7 +34,8 @@ def request(url: str, payload: Optional[dict]=None) -> Response:
 def load_file(path: str) -> str:
     path = path.split("file://")[-1]
     contents = ""
-    with open(path, "r") as file:
+    encoding = detect(open(path, "rb").read())["encoding"]
+    with open(path, "r", encoding=encoding) as file:
         lines = file.readlines()
         contents = "".join(lines)
     return contents
