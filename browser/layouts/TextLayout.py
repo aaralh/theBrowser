97d877be466e993a42e681cd43a85cf3fcbf56ce
Aaro Alhainen <aaro.alhainen@gmail.com>
2023-05-21 08:53:01 +0300

    Add support for percent unit

--- browser/layouts/TextLayout.py
+++ browser/layouts/TextLayout.py
@@ -26,11 +26,14 @@ class TextLayout(Layout):
         weight = self.node.style["font-weight"]
         style = self.node.style["font-style"]
         if style == "normal": style = "roman"
-        if not str(self.node.style["font-size"]).endswith("px"):
+        if str(self.node.style["font-size"]).endswith("px"):
+            size = int(float(self.node.style["font-size"][:-2]) * .75)
+        elif str(self.node.style["font-size"]).endswith("%"):
+            multiplier = int(self.node.style["font-size"][:-1]) / 100
+            size = int(self.parent.font_size * multiplier)
+        else:
             # This is just a temporary default value.
             size = int(float(16) * .75)
-        else:
-            size = int(float(self.node.style["font-size"][:-2]) * .75)
         self.font = get_font(size, font_weight_to_string(weight), style)
 
         self.width = self.font.measure(self.word)
